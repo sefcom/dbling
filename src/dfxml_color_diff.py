2ae269093f054abfd5b7e1b4eb71cabb44e62872
Mike Mabey <mike611@gmail.com>
2015-05-27 15:25:59 -0700

    Outputs info on duplicates. Uses lxml library for much better performance.

--- src/dfxml_color_diff.py
+++ src/dfxml_color_diff.py
@@ -5,13 +5,13 @@ graphviz-dev
 python-pydot
 """
 
-from bs4 import BeautifulSoup
-from bs4.element import Tag
 from colorama import init, Back, Fore  # BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET
 from copy import deepcopy as copy
 from hashlib import sha256
+from io import StringIO
 import json
 import logging
+from lxml import etree
 from matplotlib import pyplot as plt
 import networkx
 import os
@@ -25,7 +25,8 @@ DIRECTORY = 2
 DT_DIR = 4
 DT_REG = 8
 MAX_FILES = 2
-INODE_ONLY = True
+INODE_ONLY = False
+HASH_LABEL = True
 
 
 def _color_it(text, color, b):
@@ -55,6 +56,32 @@ def make_cyan(text, b=True):
 
 def make_black(text, b=True):
     return _color_it(text, 'BLACK', b)
+
+
+class FileObj(object):
+
+    def __init__(self, element_obj, namespace=''):
+        self._obj = element_obj
+        self._ns = namespace
+
+    def findtext(self, element_path):
+        val = self._obj.findtext(self._ns + element_path)
+        if val is None:
+            raise AttributeError
+        return val
+
+    def find(self, element_path):
+        return self._obj.find(self._ns + element_path)
+
+    def iter_grandchild(self, child, grandchild):
+        c = self.find(child)
+        if c is None:
+            return []
+        return c.iterfind(self._ns + grandchild)
+
+    @property
+    def obj(self):
+        return self._obj
 
 
 class ColorDiff(object):
@@ -153,9 +180,10 @@ class ColorDiff(object):
         :rtype: None
         """
         logging.info('Beginning import from file: %s' % file_path)
-        with open(file_path) as xml_file:
-            soup = BeautifulSoup(xml_file.read(), "xml")
-        logging.info('DFXML converted to soup. Importing into a graph.')
+        xml_tree_root = etree.parse(file_path).getroot()
+        ns = '{http://www.forensicswiki.org/wiki/Category:Digital_Forensics_XML}'
+        vol = xml_tree_root.find(ns + 'volume')
+        logging.info('DFXML converted to element tree. Importing into a graph.')
 
         ex_pat1 = re.compile('/\.\.?$')
         ex_pat2 = re.compile('^/?home/\.shadow/(.+)')
@@ -175,6 +203,9 @@ class ColorDiff(object):
 
         edges_to_add = []
         num_skipped_files = 0
+        num_duplicate_parent_dirs = 0
+        num_unallocated = 0
+        num_unused = 0
 
         # Figure out which file this is: 1, 2, or 3
         file_num = None
@@ -185,8 +216,50 @@ class ColorDiff(object):
                 file_num = i  # This is the one we want
                 break
 
-        for file_obj in soup.find_all('fileobject'):
-            filename = file_obj.filename.string
+        for file_obj in vol.iterfind(ns + 'fileobject'):
+            file_obj = FileObj(file_obj, ns)
+
+            # Get the filename
+            try:
+                filename = file_obj.findtext('filename')
+            except AttributeError:
+                filename = None
+
+            # Get allocation status
+            try:
+                alloc = file_obj.findtext('alloc')
+            except AttributeError:
+                try:
+                    alloc = file_obj.findtext('unalloc')
+                except AttributeError:
+                    logging.critical('File object has neither an alloc or unalloc tag: %s' % filename)
+                    raise
+                else:
+                    alloc = str(1 - int(alloc))
+            if alloc == '0':
+                # TODO: Dr. Ahn wants these files to be included for some reason
+                num_unallocated += 1
+                continue
+
+            # Get used status
+            try:
+                used = file_obj.findtext('used')
+            except AttributeError:
+                try:
+                    used = file_obj.findtext('unused')
+                except AttributeError:
+                    logging.critical('File object has neither a used or unused tag: %s' % filename)
+                    raise
+                else:
+                    used = str(1 - int(used))
+            if used == '0':
+                num_unused += 1
+                continue
+
+            # TODO: Figure out what to do with any files that passed tests up to this point but don't have a name
+            if filename is None:
+                filename = etree.tostring(file_obj.find('id'))
+                print(filename)  # TODO: Remove this
 
             # Exclude all files that end in '/.' or '/..'
             if re.search(ex_pat1, filename):
@@ -202,42 +275,25 @@ class ColorDiff(object):
                 # Exclude all files that don't start with 'home/.shadow/'
                 continue
 
-            # Get allocation status
-            try:
-                alloc = file_obj.alloc.string
-            except AttributeError:
-                try:
-                    alloc = file_obj.unalloc.string
-                except AttributeError:
-                    logging.CRITICAL('File object has neither an alloc or unalloc tag: %s' % filename)
-                    raise
-                else:
-                    alloc = str(1 - int(alloc))
-            if alloc == '0':
-                # TODO: Dr. Ahn wants these files to be included for some reason
-                continue
-
             # Extract all pertinent information
-            inode_num = None
-            for c in file_obj.children:
-                if isinstance(c, Tag) and c.name == 'inode':
-                    inode_num = int(c.string)
-                    break
-            if inode_num is None:
-                raise ValueError("Following file has no inode number: %s" % filename)
+            inode_num = int(file_obj.findtext('inode'))
 
             basename = path.basename(filename)
-            parent_obj = int(file_obj.parent_object.inode.string)
-            meta_type = int(file_obj.meta_type.string)
+            parent_obj = int(file_obj.find('parent_object').findtext(ns + 'inode'))
+            meta_type = int(file_obj.findtext('meta_type'))
             self.type_count[meta_type] += 1
             encrypted = bool(re.search(enc_pat5, filename))
-            inode_paths[inode_num] = basename  # TODO: Set this to whatever the node label ends up being
             filename_id = sha256(filename).hexdigest()
 
             fs_offset = float('inf')
-            for fs in file_obj.find_all('byte_run'):
+            for fs in file_obj.iter_grandchild('byte_runs', 'byte_run'):
                 # Get the lowest offset of the file
-                _off = int(fs['fs_offset'])
+                _off = fs.get('fs_offset')
+                if _off is None:
+                    continue
+                else:
+                    _off = int(_off)
+
                 if _off < fs_offset:
                     fs_offset = _off
             if fs_offset == float('inf'):
@@ -246,11 +302,11 @@ class ColorDiff(object):
             filesize = '?'
             try:
                 # Try to use the DFXML-computed length first
-                filesize = file_obj.filesize.string
+                filesize = file_obj.findtext('filesize')
             except AttributeError:
                 # Iterate through the byte runs and sum their lengths
                 _sum = 0
-                for r in file_obj.find_all('byte_run'):
+                for r in file_obj.iter_grandchild('byte_runs', 'byte_run'):
                     _sum += int(r['len'])
                 if _sum > 0:
                     filesize = _sum
@@ -260,35 +316,41 @@ class ColorDiff(object):
                      # "filename": filename,
                      "filename_id": filename_id,
                      "filename_end": filename[-13:],
-                     "name_type": file_obj.name_type.string,
+                     "name_type": file_obj.findtext('name_type'),
                      "type": meta_type,
                      "alloc": alloc,
+                     "used": used,
                      "fs_offset": fs_offset,
                      "filesize": filesize,
                      }
 
-
             # Stubborn parameters
-            for k, o in (("size", file_obj.filesize),
-                         ("mode", file_obj.mode),
-                         ("used", file_obj.used),
-                         ("uid", file_obj.uid),
-                         ("gid", file_obj.gid),
-                         ("nlink", file_obj.nlink),
-                         ("mtime", file_obj.mtime),
-                         ("ctime", file_obj.ctime),
-                         ("atime", file_obj.atime),
-                         ("crtime", file_obj.crtime)):
+            for k in ("size",
+                      "mode",
+                      "uid",
+                      "gid",
+                      "nlink",
+                      "mtime",
+                      "ctime",
+                      "atime",
+                      "crtime"):
                 try:
-                    attrs[k] = o.string
+                    attrs[k] = file_obj.findtext(k)
                 except AttributeError:
                     attrs[k] = '?'
 
             # Store information about the node
             if INODE_ONLY:
                 _id = inode_num
+            elif HASH_LABEL:
+                _id = filename_id
             else:
                 _id = basename
+
+            if inode_num in inode_paths and inode_paths[inode_num] != _id:
+                num_duplicate_parent_dirs += 1
+            else:
+                inode_paths[inode_num] = _id
 
             if meta_type == FILE:
                 if encrypted:
@@ -316,14 +378,19 @@ class ColorDiff(object):
             if not skip_add_edge:
                 edges_to_add.append((parent_obj, _id))
 
-        logging.info("Done importing. Number of skipped files: %d" % num_skipped_files)
-        logging.info("File count by type: %s" % self.type_count)
+        logging.info("Done importing.")
+        logging.debug("Number of skipped files: %d" % num_skipped_files)
+        logging.debug("Number of unallocated files: %d" % num_unallocated)
+        logging.debug("Number of allocated but unused files: %d" % num_unused)
+        logging.debug("Number of duplicate parent directory entries: %d" % num_duplicate_parent_dirs)
+        logging.debug("File count by type: %s" % self.type_count)
         type_sum = 0
         for i in self.type_count:
             type_sum += self.type_count[i]
         logging.info("Total file objects: %d" % type_sum)
 
         if file_num == 1:
+            # TODO: Make this optional (CLI param) and output to a file instead of stdout
             print("\nDuplicates:"),
             dstring = ' '
             d_fields = (('inode', '6'), ('parent_inode', '6'), ('name_type', '2'), ('type', '2'), ('alloc', '2'),
@@ -335,7 +402,7 @@ class ColorDiff(object):
             header = dstring % ("inode", "pinode", "nt", "ty", "al", "ud", "mode", "nlk", "uid", "gid", "Start",
                                 "Length", "Modified Time", "inode Changed Time", "Accessed Time", "Created Time",
                                 "Filename Hash Tail", "Filename Tail")
-            break_str = ('  -- ' * ((len(header) / 5) + 1))[:len(header)]
+            break_str = make_green(('--   ' * ((len(header) / 5) + 1))[:len(header)], 0)
             header = make_yellow(make_black(header, 0))
             line_count = 0
             for x, y in duplicates:
@@ -370,13 +437,13 @@ class ColorDiff(object):
 
                 print(dstring % tuple(x_fields))
                 print(dstring % tuple(y_fields))
-                print(make_green(break_str, 0)),
+                print(break_str),
             raise KeyboardInterrupt
 
         for u, v in edges_to_add:
             if INODE_ONLY:
                 self.digr.add_edge(u, v)
-            else:
+            else:  # Includes when HASH_LABEL == True
                 self.digr.add_edge(inode_paths[u], v)
 
         self.files[file_num] = (set(encrypted_files),
@@ -384,6 +451,9 @@ class ColorDiff(object):
                                 set(unencrypted_files),
                                 set(unencrypted_directories))
         self.unknown_type_files += other_files
+
+    def trim_unuseful(self):
+
 
 
 def main():
