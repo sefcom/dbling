99961f1cefe274a4434951a55cb57b074913c2c8
Mike Mabey <mike611@gmail.com>
2017-09-21 10:11:47 -0700

    Doing away with the old version of gripper

--- drive_api/api_connectors/__init__.py
+++ drive_api/api_connectors/__init__.py
@@ -6,3 +6,17 @@ from api_connectors.people import PeopleAPI
 from api_connectors.plus import PlusAPI
 
 __all__ = ['DriveAPI', 'GSuiteDirectoryAPI', 'AdminAPI', 'GmailAPI', 'PeopleAPI', 'PlusAPI']
+
+
+def get_api(api, **kwargs):
+    a = {'drive': DriveAPI,
+         'plus': PlusAPI,
+         'people': PeopleAPI,
+         'dir': GSuiteDirectoryAPI,
+         'gmail': GmailAPI,
+         'admin': AdminAPI,
+         }
+    try:
+        return a[api](**kwargs)
+    except KeyError:
+        raise ValueError('Specified API must be one of:\n{}'.format(list(a.keys())))
